a4429e58b2c476e53d163931fb3876d76629218c
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-06-09 20:44:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 if (window.innerWidth < 768) {
-	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-		elem.classList.remove('animated');
-		elem.removeAttribute('data-bss-hover-animate');
-		elem.removeAttribute('data-aos');
-		elem.removeAttribute('data-bss-parallax-bg');
-		elem.removeAttribute('data-bss-scroll-zoom');
-	});
+  [].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach((elem) => {
+    elem.classList.remove('animated');
+    elem.removeAttribute('data-bss-hover-animate');
+    elem.removeAttribute('data-aos');
+    elem.removeAttribute('data-bss-parallax-bg');
+    elem.removeAttribute('data-bss-scroll-zoom');
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-
-	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
-	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
-		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
-		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
-	});
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+  hoverAnimationTriggerList.forEach((hoverAnimationEl) => {
+    hoverAnimationEl.addEventListener('mouseenter', (e) => { e.target.classList.add('animated', e.target.dataset.bssHoverAnimate); });
+    hoverAnimationEl.addEventListener('mouseleave', (e) => { e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate); });
+  });
 }, false);
